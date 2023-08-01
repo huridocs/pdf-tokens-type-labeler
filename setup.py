fc899ba7bbb610f99349eeb61de13f76f6e47623
Gabo <gabriel.piles.glez@gmail.com>
2023-08-01 16:10:15 +0200

    Try setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import setuptools
 from setuptools import setup
 
 with open('requirements.txt') as f:
@@ -8,11 +7,12 @@ PROJECT_NAME = 'pdf-tokens-type-labeler'
 
 setup(
     name=PROJECT_NAME,
-    packages=setuptools.find_packages(),
+    packages=[PROJECT_NAME],
     package_dir={PROJECT_NAME: 'src'},
-    version='0.4',
+    version='0.5',
     url='https://github.com/huridocs/pdf-tokens-type-labeler',
     author='HURIDOCS',
     description='This tool returns each token type inside a PDF',
     install_requires=requirements
 )
+
