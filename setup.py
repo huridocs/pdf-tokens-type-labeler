384a5134d91e173974d32d1e906170d76d77ecaf
Gabo <gabriel.piles.glez@gmail.com>
2023-08-01 17:05:13 +0200

    Try setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ PROJECT_NAME = 'pdf-tokens-type-labeler'
 setup(
     name=PROJECT_NAME,
     packages=[PROJECT_NAME],
-    package_dir={PROJECT_NAME: 'src'},
+    package_dir={PROJECT_NAME: 'src', 'pdf_features': 'src/pdf_features'},
     version='0.5',
     url='https://github.com/huridocs/pdf-tokens-type-labeler',
     author='HURIDOCS',
