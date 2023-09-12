7034b3ca4cb670f1cf5748521719b8f7141040b2
Ali Altıparmak <ali@huridocs.org>
2023-09-12 19:34:01 +0300

    Update pdf tokens type model

--- src/pdf_tokens_type_trainer/download_models.py
+++ src/pdf_tokens_type_trainer/download_models.py
@@ -3,7 +3,7 @@ from huggingface_hub import hf_hub_download
 pdf_tokens_type_model = hf_hub_download(
     repo_id="HURIDOCS/pdf-segmetation",
     filename="pdf_tokens_type.model",
-    revision="2605abe101cab67c3d13e21ea9e80e6e55376d62",
+    revision="87895b77811d11d89efa71861ca7a35e1c34bf47",
 )
 
 token_type_finding_config_path = hf_hub_download(
