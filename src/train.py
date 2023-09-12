e1a4e55f4c99208a3758c1f178afdacefae64cd8
Ali Altıparmak <ali@huridocs.org>
2023-09-12 19:33:30 +0300

    Update train script

--- src/train.py
+++ src/train.py
@@ -1,19 +1,16 @@
-from os.path import join
-from pathlib import Path
-
-
 from pdf_token_type_labels.load_labeled_data import load_labeled_data
 from pdf_tokens_type_trainer.ModelConfiguration import ModelConfiguration
 from pdf_tokens_type_trainer.TokenTypeTrainer import TokenTypeTrainer
-from pdf_tokens_type_trainer.config import TRAINED_MODEL_PATH, PDF_TOKENS_TYPE_ROOT_PATH, PDF_LABELED_DATA_ROOT_PATH
+from pdf_tokens_type_trainer.config import TRAINED_MODEL_PATH, PDF_LABELED_DATA_ROOT_PATH
 
 
-def train(filter_in: str = None):
-    Path(join(PDF_TOKENS_TYPE_ROOT_PATH, TRAINED_MODEL_PATH)).parent.mkdir(exist_ok=True)
-    pdfs_features = load_labeled_data(PDF_LABELED_DATA_ROOT_PATH, filter_in)
+def train():
     model_configuration = ModelConfiguration()
-    trainer = TokenTypeTrainer(pdfs_features, model_configuration)
-    trainer.train(TRAINED_MODEL_PATH)
+    train_pdf_features = load_labeled_data(PDF_LABELED_DATA_ROOT_PATH)
+    trainer = TokenTypeTrainer(train_pdf_features, model_configuration)
+    labels = [token.token_type.get_index() for token in trainer.loop_tokens()]
+    print("training")
+    trainer.train(TRAINED_MODEL_PATH, labels)
 
 
 if __name__ == "__main__":
