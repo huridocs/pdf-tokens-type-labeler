738a48fca6ff7c8fee0fe8427cb6f38bd20658b8
Ali Altıparmak <ali@huridocs.org>
2023-09-12 15:46:19 +0300

    Fix path

--- src/pdf_token_type_labels/load_labeled_data.py
+++ src/pdf_token_type_labels/load_labeled_data.py
@@ -2,15 +2,15 @@ from os import listdir
 from os.path import join, isdir
 
 from pdf_features.PdfFeatures import PdfFeatures
-from pdf_tokens_type_trainer.config import TOKEN_TYPE_LABEL_PATH, PDF_LABELED_DATA_ROOT_PATH
+from pdf_tokens_type_trainer.config import TOKEN_TYPE_RELATIVE_PATH
 
 
-def loop_datasets(filter_in: str):
-    for dataset_name in listdir(TOKEN_TYPE_LABEL_PATH):
+def loop_datasets(token_type_labeled_data_path: str, filter_in: str):
+    for dataset_name in listdir(token_type_labeled_data_path):
         if filter_in and filter_in not in dataset_name:
             continue
 
-        dataset_path = join(TOKEN_TYPE_LABEL_PATH, dataset_name)
+        dataset_path = join(token_type_labeled_data_path, dataset_name)
 
         if not isdir(dataset_path):
             continue
@@ -18,19 +18,20 @@ def loop_datasets(filter_in: str):
         yield dataset_name, dataset_path
 
 
-def load_labeled_data(filter_in: str = None) -> list[PdfFeatures]:
+def load_labeled_data(pdf_labeled_data_root_path: str, filter_in: str = None) -> list[PdfFeatures]:
     if filter_in:
         print(f"Loading only datasets with the key word: {filter_in}")
         print()
 
     pdfs_features: list[PdfFeatures] = list()
+    token_type_labeled_data_path: str = join(pdf_labeled_data_root_path, TOKEN_TYPE_RELATIVE_PATH)
 
-    for dataset_name, dataset_path in loop_datasets(filter_in):
+    for dataset_name, dataset_path in loop_datasets(token_type_labeled_data_path, filter_in):
         print(f"loading {dataset_name} from {dataset_path}")
 
         dataset_pdf_name = [(dataset_name, pdf_name) for pdf_name in listdir(dataset_path)]
         for dataset, pdf_name in dataset_pdf_name:
-            pdf_features = PdfFeatures.from_labeled_data(PDF_LABELED_DATA_ROOT_PATH, dataset, pdf_name)
+            pdf_features = PdfFeatures.from_labeled_data(pdf_labeled_data_root_path, dataset, pdf_name)
             pdfs_features.append(pdf_features)
 
     return pdfs_features
