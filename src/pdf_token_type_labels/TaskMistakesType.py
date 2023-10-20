cfad32a2dcca7e899bad43608712ccb3e9e30737
Gabo <gabriel.piles.glez@gmail.com>
2023-10-20 13:13:20 +0200

    Add contains to enum

--- src/pdf_token_type_labels/TaskMistakesType.py
+++ src/pdf_token_type_labels/TaskMistakesType.py
@@ -5,3 +5,7 @@ class TaskMistakesType(Enum):
     CORRECT = "CORRECT"
     WRONG = "WRONG"
     MISSING = "MISSING"
+
+    @staticmethod
+    def contains(key: str):
+        return key.upper() in [e.value for e in TaskMistakesType]
