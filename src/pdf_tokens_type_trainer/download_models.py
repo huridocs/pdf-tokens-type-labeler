df13c8b628b5023a24b65e658aef435f20a4abaa
Ali Altıparmak <ali@huridocs.org>
2023-12-08 16:20:02 +0300

    Fix huggingface repo id

--- src/pdf_tokens_type_trainer/download_models.py
+++ src/pdf_tokens_type_trainer/download_models.py
@@ -1,19 +1,19 @@
 from huggingface_hub import hf_hub_download
 
 pdf_tokens_type_model = hf_hub_download(
-    repo_id="HURIDOCS/pdf-segmetation",
+    repo_id="HURIDOCS/pdf-segmentation",
     filename="pdf_tokens_type.model",
     revision="87895b77811d11d89efa71861ca7a35e1c34bf47",
 )
 
 token_type_finding_config_path = hf_hub_download(
-    repo_id="HURIDOCS/pdf-segmetation",
+    repo_id="HURIDOCS/pdf-segmentation",
     filename="tag_type_finding_model_config.txt",
     revision="7d98776dd34acb2fe3a06495c82e64b9c84bdc16",
 )
 
 letter_corpus_path = hf_hub_download(
-    repo_id="HURIDOCS/pdf-segmetation",
+    repo_id="HURIDOCS/pdf-segmentation",
     filename="letter_corpus.txt",
     revision="da00a69c8d6a84493712e819580c0148757f466c",
 )
