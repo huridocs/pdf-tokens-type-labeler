7f46632cff996baaf42b8c8ac96bab20ab59dd32
gabriel-piles <gabriel.piles.glez@gmail.com>
2024-02-12 14:32:24 +0100

    Unicode features (#25)
    * Add features regarding token letters unicode categories
    
    * Update version
    
    * Test unicode token type model
    
    * Test unicode token type model
    
    * Create model with all the data
    
    * Update token type  model
    
    * Update token type model
    
    * Update version

--- src/pdf_tokens_type_trainer/download_models.py
+++ src/pdf_tokens_type_trainer/download_models.py
@@ -3,17 +3,11 @@ from huggingface_hub import hf_hub_download
 pdf_tokens_type_model = hf_hub_download(
     repo_id="HURIDOCS/pdf-segmentation",
     filename="pdf_tokens_type.model",
-    revision="87895b77811d11d89efa71861ca7a35e1c34bf47",
+    revision="c71f833500707201db9f3649a6d2010d3ce9d4c9",
 )
 
 token_type_finding_config_path = hf_hub_download(
     repo_id="HURIDOCS/pdf-segmentation",
     filename="tag_type_finding_model_config.txt",
     revision="7d98776dd34acb2fe3a06495c82e64b9c84bdc16",
-)
-
-letter_corpus_path = hf_hub_download(
-    repo_id="HURIDOCS/pdf-segmentation",
-    filename="letter_corpus.txt",
-    revision="da00a69c8d6a84493712e819580c0148757f466c",
 )
